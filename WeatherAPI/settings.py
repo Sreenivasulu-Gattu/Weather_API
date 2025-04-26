e6fa6f8e5ecae6b05579c0c7c4f8a22237065f4b
Sreenivasulu Gattu <sreenivasgattu12345@gmail.com>
2025-04-26 23:57:24 +0530

    Add STATIC_ROOT setting for production in settings.py

--- WeatherAPI/settings.py
+++ WeatherAPI/settings.py
@@ -126,6 +126,8 @@ STATIC_URL = 'static/'
 
 
 STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   # for production collectstatic
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
